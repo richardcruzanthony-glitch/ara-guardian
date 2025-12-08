e5fb3ba9064a6d5fb4f14caf9a846e41dc266274
Richard Cruz Anthony <richardcruzanthony@gmail.com>
2025-12-07 23:30:29 -0800

    Clean successful build

--- .mastra/output/tools.mjs
+++ .mastra/output/tools.mjs
@@ -1,10 +1,10 @@
-import * as tool0 from './tools/5f449739-cf51-4d8d-b0b2-ef5c29f5ba19.mjs';
-import * as tool1 from './tools/e05843fe-34cb-4374-8cd5-16f7657eb3b0.mjs';
-import * as tool2 from './tools/8f28cdd4-4409-43f1-a1d2-1b17ea1476ff.mjs';
-import * as tool3 from './tools/5b1b3785-17b3-4bcc-a0cd-b244393ee220.mjs';
-import * as tool4 from './tools/143d4e8d-9653-4ed4-9792-7c1058396462.mjs';
-import * as tool5 from './tools/f3d7eac0-078e-4359-9ac5-8f741516f3d0.mjs';
-import * as tool6 from './tools/3b600a01-dbc2-4cfd-998e-140c39b69812.mjs';
-import * as tool7 from './tools/24e7723d-f768-4367-9946-b9f9cd7805a9.mjs';
+import * as tool0 from './tools/8d77cc50-5bb6-4e00-b39b-79d897ea5678.mjs';
+import * as tool1 from './tools/7daa428e-8a0f-4f3b-95de-e99a2c0e9fa8.mjs';
+import * as tool2 from './tools/af826a6a-63a5-481b-b82f-9f321d17e4d8.mjs';
+import * as tool3 from './tools/20176935-dada-4263-b46e-219bbe7bb842.mjs';
+import * as tool4 from './tools/65dfc3e3-5ff7-44cd-8a1a-47697b031ce4.mjs';
+import * as tool5 from './tools/799342c7-a945-4987-97a3-91708bbecbaf.mjs';
+import * as tool6 from './tools/3d57045c-10db-49bd-83c0-87e223b83539.mjs';
+import * as tool7 from './tools/21527cce-dbee-479b-a0e0-32939030576f.mjs';
 
 export const tools = [tool0, tool1, tool2, tool3, tool4, tool5, tool6, tool7]
--- telemetry-config.ts
+++
@@ -1,1 +0,0 @@
-export const telemetry = { enabled: false }; export default {};
