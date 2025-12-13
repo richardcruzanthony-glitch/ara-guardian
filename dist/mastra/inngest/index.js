9c156f33aae425181529cbd64aa38db8a2746292
richardcruzanthony-glitch <richardcruzanthony@gmail.com>
2025-12-13 13:13:49 -0800

    build: rebuild production bundle for Render.com deployment

--- dist/mastra/inngest/index.js
+++ dist/mastra/inngest/index.js
@@ -6,16 +6,3 @@ export const inngestServe = serve({
     client: inngest,
     functions: [],
 });
-Set-Location C:\Users\guardian\Downloads
-gh repo clone richardcruzanthony-glitch/ara-guardian
-
-export default inngestServe;
-npm install
-npm run build
-npm start
-cd ..
-ngrok http 3000
-ngrok authtoken 2YdX5fY7bX5_7G3zYv8J8vX4F6h_3Z5bX7G3zYv8J8vX4F6h_3Z5bX7G3zYv8J8vX4F6h_3Z5bX7G3zYv8J8vX4F6h_3Z5b     Set-Location C:\Users\guardian\Downloads
-gh repo clone richardcruzanthony-glitch/ara-guardian
-npm install
-                
